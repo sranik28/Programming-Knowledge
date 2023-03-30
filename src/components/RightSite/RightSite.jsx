a4a4dc98034068c11eab8f538b55bf28f0d167e1
sranik28~ <anikshariarrahman@gmail.com>
2023-03-31 02:45:43 +0600

    Rright Site Card added

---
+++ src/components/RightSite/RightSite.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightSite = () => {
+    return (
+        <div className='my-10'>
+            <h1 className='font-bold text-2xl'>Bookmarked Blogs : </h1>
+        </div>
+    );
+};
+
+export default RightSite;
